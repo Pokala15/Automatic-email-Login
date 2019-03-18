89ff0ac0bbc8fb6006448d1ad151a4f92dd26eb4
Pokala15 <47535174+Pokala15@users.noreply.github.com>
2019-03-18 18:38:24 +0530

    initial commit

---
+++ login.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By as b
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver=webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession")
+email_ele = driver.find_element(b.ID,"identifierId")
+email_ele.send_keys("pokalabhargavi99@gmail.com")
+next_but1= driver.find_element(b.ID,"identifierNext")
+next_but1.click()
+time.sleep(2)
+#driver.implicitly_wait(5)
+#WebDriverWait(driver,5).until(EC.presence_of_element_located((b.NAME,"password")))
+pass_ele = driver.find_element(b.NAME,"password")
+pass_ele.send_keys("Likhith@01")
+pass_next = driver.find_element(b.ID,"passwordNext")
+pass_next.click()
